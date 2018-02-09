7b582bd861e1c52f78a0c41fd10637254ecb13e6
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2018-02-09 12:44:19 +0300

    cutText final

--- function/functionCutText.php
+++ function/functionCutText.php
@@ -10,3 +10,10 @@ function cutText($text, $numLetters)
 
   return $text;
 }
+
+$gif = [
+  'gif' => '/img/cat.gif',
+  'title' => 'Yachtec was built on a love of yachting. Founded in 1984 by John Smith, renowned sailor and worldwide single-handed ocean racer',
+  'author' => 'frexin',
+  'likes_count' => 5
+];
